d38b8fc86a54fae81e7d0fb5f724e32bb668bebf
emeliepetersson <emepet1204@skola.goteborg.se>
2019-11-25 16:17:27 +0100

    add images to slideshow

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -23,55 +23,6 @@ changeSlideButtons.forEach(changeSlideButton => {
   changeSlideButton.addEventListener('click', plusSlides);
 });
 
-// Add touch event for mobile
-// https://stackoverflow.com/questions/2264072/detect-a-finger-swipe-through-javascript-on-the-iphone-and-android
-slideImages.forEach(slideImage => {
-  slideImage.addEventListener('touchstart', handleTouchStart, false); 
-  slideImage.addEventListener('touchmove', handleTouchMove, false);
-});
-const xDown = null;                                                        
-const yDown = null;
-
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
-
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
-
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    let xUp = evt.touches[0].clientX;                                    
-    let yUp = evt.touches[0].clientY;
-
-    let xDiff = xDown - xUp;
-    let yDiff = yDown - yUp;
-
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-          showSlides(slideIndex += -1); 
-        } else {
-          showSlides(slideIndex += 1);
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* up swipe */ 
-        } else { 
-            /* down swipe */
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};  
-
 
 // Dot image controls. Check if target element contains specified class name, then assign slideIndex new value.
 function currentSlide(event) {
@@ -87,7 +38,10 @@ function currentSlide(event) {
 
   } else if (event.target.classList.contains('four')) {
     showSlides(slideIndex = 4);
-
+  } else if (event.target.classList.contains('five')) {
+    showSlides(slideIndex = 5);
+  } else if (event.target.classList.contains('six')) {
+    showSlides(slideIndex = 6);
   }
 }
 
