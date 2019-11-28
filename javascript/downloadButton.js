4b8be5ae3b942320c0e852ce6f6bbfc49ebf20aa
Marcus <MarcusIsCode@users.noreply.github.com>
2019-11-28 20:26:18 +0100

    uppdate
    download/pdf/folder

--- javascript/downloadButton.js
+++ javascript/downloadButton.js
@@ -3,10 +3,10 @@
 // Click function for downloading files
 const downloadButton = document.getElementById('downloadScroll');
 downloadButton.addEventListener('click',()=>{
-    window.open('suggestions/download-button/test.pdf','_blank');
+    window.open('PDF/Blackstar Folder Final.pdf','_blank');
 }) 
 
 const downloadInDesktop = document.getElementById('downloadDownload');
 downloadInDesktop.addEventListener('click',()=>{
-    window.open('suggestions/download-button/test.pdf','_blank');
+    window.open('PDF/Blackstar Folder Final.pdf','_blank');
 }) 
