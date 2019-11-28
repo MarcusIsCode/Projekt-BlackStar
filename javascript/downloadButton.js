333935a41338ce488f9757d65bfb1d675b62fd66
emeliepetersson <emepet1204@skola.goteborg.se>
2019-11-28 15:27:00 +0100

    add eventlistener

--- javascript/downloadButton.js
+++ javascript/downloadButton.js
@@ -5,3 +5,8 @@ const downloadButton = document.getElementById('downloadScroll');
 downloadButton.addEventListener('click',()=>{
     window.open('suggestions/download-button/test.pdf','_blank');
 }) 
+
+const downloadInDesktop = document.getElementById('downloadDownload');
+downloadInDesktop.addEventListener('click',()=>{
+    window.open('suggestions/download-button/test.pdf','_blank');
+}) 
